fd0709f8c537e311bae5315d008566a4c0955b61
Juan <j.causil@udea.edu.co>
2026-09-20 13:19:39 -0500

    Ultima revision del archivo

--- Funciones_bit.h
+++ Funciones_bit.h
@@ -3,7 +3,7 @@
 const unsigned char ficha0 = 0b000, ficha_1 = 0b001,ficha2 = 0b010,ficha_3 = 0b011;
 const unsigned char ficha_4 = 0b100, ficha_5 = 0b101, Libre= 0b110, nulo = 0b111;
 
-int Bytes_nec(int rows, int columns);
+int Bytes_nec(int fila, int columa);
 unsigned char *Pack_memoria(int bytes_rser);
 void Delete_memoria(unsigned char *&memory);
 unsigned char Obtener_ficha(const unsigned char *memory, int Capacid_bytes, int Posicion);
